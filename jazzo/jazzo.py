7787da0a35fe6671c183608dca20b220c019022a
jazzzooo <38244149+jazzzooo@users.noreply.github.com>
2024-01-06 15:30:25 -0800

    constant white advantage

--- jazzo/jazzo.py
+++ jazzo/jazzo.py
@@ -10,19 +10,52 @@ SMALL_JUMP = 1.6
 LARGE_JUMP = 33
 LARGE_EVERY = 21
 
-STEPS = 2500
+STEPS = 1200
+
+# White advantage constant, based on the number of wins in lichess data up to November 2023
+# Elo advantage can be calculated as 400 * (log10(W + D/2) - log10(B + D/2))
+# TimeControl              W         B        D   ≈elo
+# UltraBullet       14988707  14323116   477377  7.764
+# Bullet           784325525 740605279 48396796  9.657
+# Blitz           1054975859 978840645 92865717 12.444
+# Rapid            299734348 275830958 27386294 13.781
+# Classical         57414088  52809117  5140023 13.876
+# Correspondence     4474613   4241503   629812  8.668
+W = 1054975859
+B = 978840645
+D = 92865717
+WA = (W + D / 2) / (B + D / 2)
 
 # Load densely packed data
 with open("data/blitz.packed", "rb") as f:
+    # Number of players
     n_players = int.from_bytes(f.read(4), "little")
+
+    # For each player, the number of different opponents that they faced
     n_opponents = np.fromfile(f, dtype=np.uint16, count=n_players).astype(np.intc)
+    n_pairings = n_opponents.sum()
+
+    # The offsets in the opponents and totals arrays to see how many games each player had
+    # against a specific opponent
     indices = np.zeros(n_players + 1, dtype=np.intc)
     indices[1:] = np.cumsum(n_opponents)
-    n_pairings = n_opponents.sum()
-    totals_inv = 1 / np.fromfile(f, dtype=np.uint16, count=n_players).astype(np.float64)
+
+    # The total number of games played by each player. We precompute the inverse to avoid dividing later
+    totals_inv = 1 / (np.fromfile(f, dtype=np.uint16, count=n_players).astype(np.float64))
+
+    # How many points total did the player score
     scored = np.fromfile(f, dtype=np.uint16, count=n_players).astype(np.float64) / 2
-    opponents = np.ascontiguousarray(np.fromfile(f, dtype=np.uint32, count=n_pairings).astype(np.intc))
-    opp_played = np.ascontiguousarray(np.fromfile(f, dtype=np.uint16, count=n_pairings).astype(np.intc))
+
+    # Now the big arrays:
+    # Indexes of the opponents faced by each player.
+    # player_i will have faced players with ids opponents[indices[i]:indices[i+1]]
+    opponents = np.fromfile(f, dtype=np.uint32, count=n_pairings).astype(np.intc)
+    # The total number of games played by each opponent of each player, as white and as black
+    white_opp_totals = np.fromfile(f, dtype=np.uint16, count=n_pairings).astype(np.intc)
+    black_opp_totals = np.fromfile(f, dtype=np.uint16, count=n_pairings).astype(np.intc)
+
+    # Optionally, load the player ids
+    ids = [f.read(26).rstrip(b"\0").decode() for b in range(n_players)]
 
 
 def optimize(steps=STEPS, small_jump=SMALL_JUMP, large_jump=LARGE_JUMP, large_every=LARGE_EVERY, *, verbose=True):
@@ -30,14 +63,14 @@ def optimize(steps=STEPS, small_jump=SMALL_JUMP, large_jump=LARGE_JUMP, large_ev
     # log10(rating) * 400 + 1500
     # altho the median will turn out not to be 1500 usually.
     # maybe the median or even mode rating should be added instead?
-    ratings = np.ones(n_players, dtype=np.float64, order="C")
-    errors = np.zeros(n_players, dtype=np.float64, order="C")
+    ratings = np.ones(n_players, dtype=np.float64)
+    errors = np.zeros(n_players, dtype=np.float64)
     # also we dont actually need to start with 1500 ratings for everyone
     # seeding any kind of ratings will make it converge 100x faster
     iterator = tqdm(range(steps), smoothing=0) if verbose else range(steps)
     for i in iterator:
-        prediction_errors(errors, ratings, scored, opponents, opp_played, indices, n_players)
-        adjust(ratings, errors, totals_inv, n_players, small_jump if i % int(large_every) else large_jump)
+        prediction_errors(errors, ratings, scored, opponents, white_opp_totals, black_opp_totals, indices, WA)
+        adjust(ratings, errors, totals_inv, small_jump if i % int(large_every) else large_jump)
         loss = np.abs(errors).sum()
         if verbose:
             iterator.set_description(f"loss: {loss:16.5f}")
@@ -49,3 +82,7 @@ def optimize(steps=STEPS, small_jump=SMALL_JUMP, large_jump=LARGE_JUMP, large_ev
 
 if __name__ == "__main__":
     ratings, loss = optimize()
+
+players = dict(zip(ids, np.log10(ratings) * 400 + 1500))
+for i in sorted(players, key=players.get, reverse=True)[:10]:
+    print(f"{i:26} {players[i]:.3f}")
--- jazzo/prediction_errors.pyx
+++ jazzo/prediction_errors.pyx
@@ -6,19 +6,26 @@
 from cython.parallel import prange
 
 
-def prediction_errors(double[::1] errors, double[::1] ratings, double[::1] scored, int[::1] opponents, int[::1] opp_played, int[::1] indices, int n_players):
+def prediction_errors(double[::1] errors, double[::1] ratings, double[::1] scored, int[::1] opponents, int[::1] white_opp_totals, int[::1] black_opp_totals, int[::1] indices, double WA):
     cdef int j, k
-    cdef double score
+    cdef double score, rating_white, rating_black, opp_white, opp_black
 
-    for j in prange(n_players, nogil=True, schedule='guided'):
+    for j in prange(errors.shape[0], nogil=True, schedule='guided'):
         score = 0.0
+        rating_white = WA * ratings[j]
+        rating_black = ratings[j]
+
         for k in range(indices[j], indices[j + 1]):
-            score = score + opp_played[k] * ratings[j] / (ratings[j] + ratings[opponents[k]]) 
+            opp_black = ratings[opponents[k]]
+            opp_white = WA * ratings[opponents[k]]
+
+            score = score + white_opp_totals[k] * rating_white / (rating_white + opp_black) \
+                          + black_opp_totals[k] * rating_black / (rating_black + opp_white) 
         errors[j] = scored[j] - score
 
 
-def adjust(double[::1] ratings, double[::1] errors, double[::1] total_inv, int n_players, double K):
+def adjust(double[::1] ratings, double[::1] errors, double[::1] total_inv, double K):
     cdef int i
     
-    for i in prange(n_players, nogil=True, schedule='guided'):
+    for i in prange(errors.shape[0], nogil=True, schedule='guided'):
         ratings[i] = ratings[i] * 10 ** (K * errors[i] * total_inv[i])
